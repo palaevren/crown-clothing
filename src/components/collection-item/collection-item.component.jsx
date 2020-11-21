2e30b95779d051691edb4a2dc1d3395f25b85a98
Evren Pala <palaevren@gmail.com>
2020-11-21 16:57:08 +0300

    adding header component and links to homepage

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -7,9 +7,9 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div className='image' style={{
             backgroundImage: `url(${imageUrl})`
         }}/>
-        <div className='collaction-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
         </div>
     </div>
 );
