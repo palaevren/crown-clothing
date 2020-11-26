cb7d1bb4170e75623c4034f4cdd3a923ed952d5f
Evren PALA <evren.pala@btk.gov.tr>
2020-11-26 09:12:06 +0300

    created carrt item component & connected cart dropdown with cart-items reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -7,5 +7,5 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             : cartItem
             )
     }
-    return [cartItems, { ...cartItemToAdd, quantity: 1}];
+    return [...cartItems, { ...cartItemToAdd, quantity: 1}];
 };
